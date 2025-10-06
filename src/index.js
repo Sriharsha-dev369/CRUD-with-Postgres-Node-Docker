782d30afea2a242fe456112f86eb2434cb5dbdf5
Ash <sriharshaanaparthi@gmail.com>
2025-10-06 18:21:33 +0530

    connect postgres db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool  from './config/db.js';
 
 dotenv.config();
 
@@ -9,9 +10,15 @@ const PORT  = process.env.PORT || 3001;
 
 //Middlewares
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 //routes
+app.get('/', async (req,res)=>{
+    const result = await pool.query('SELECT current_database()');
+    console.log(result);
+    console.log('end');
+    res.send(`the database name is :${result.rows[0].current_database}`);
+});
 
 ///Error handling middleware
 
